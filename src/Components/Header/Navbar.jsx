1e5776df89f1199c03636f46787b2b49c7990eb2
jemmy <flashyjotebele@gmail.com>
2022-11-23 10:42:15 -1100

    fixed icon responsivity

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="max-w-6xl mx-auto px-8 py-5 flex justify-between items-center">
       <p className="nav--text ">Homely</p>
-      <HiMenuAlt1 className="text-orange text-4xl hover:scale-150 duration-200 cursor-pointer" />
+      <HiMenuAlt1 className="text-orange text-4xl hover:scale-150 duration-200 cursor-pointer lg:hidden" />
       <ul className="hidden space-x-10 items-center lg:flex">
         <li className="orange-color">Home</li>
         <li>About Us</li>
