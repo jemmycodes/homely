dcbae1181aa51965316055e019e95d148dbca27d
jemmy <flashyjotebele@gmail.com>
2022-11-23 10:40:04 -1100

    lazy loading and react icons added

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,11 +1,11 @@
 import HeaderBtn from "./HeaderBtn";
-import NavbarSmallScreens from "./NavbarSmallScreens";
+import { HiMenuAlt1 } from "react-icons/hi";
 
 function Navbar() {
   return (
     <nav className="max-w-6xl mx-auto px-8 py-5 flex justify-between items-center">
       <p className="nav--text ">Homely</p>
-      <NavbarSmallScreens />
+      <HiMenuAlt1 className="text-orange text-4xl hover:scale-150 duration-200 cursor-pointer" />
       <ul className="hidden space-x-10 items-center lg:flex">
         <li className="orange-color">Home</li>
         <li>About Us</li>
